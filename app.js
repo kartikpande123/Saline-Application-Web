e01dd0f656ef3ec1169e4649cf24dd1aac79ca70
kartikpande123 <kartikpande67@gmail.com>
2025-02-24 11:48:07 +0530

    changed file names

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Existing routes
 app.get("/", (req, res) => {
-   res.sendFile(path.join(__dirname, "public", "login.html"));
+   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/home", (req, res) => {
